7059218180abf404a9e4e0d9233797902d789576
volkan <volkancaliskan14@gmail.com>
2022-04-23 03:32:36 +0300

    localStorage - Section End Exercise

--- fonksiyonlar-bolum-sonu-egzersizi/index.js
+++ fonksiyonlar-bolum-sonu-egzersizi/index.js
@@ -1,29 +1,33 @@
-let counter = 0;
+let counter = localStorage.getItem("counter") ? Number(localStorage.getItem("counter")) : 0;
 let counterDOM = document.querySelector('#counter')
 let increaseDOM = document.querySelector('#increase')
 let decreaseDOM = document.querySelector('#decrease')
 
 counterDOM.innerHTML = counter
 
+
 increaseDOM.addEventListener("click", clickEvent)
 decreaseDOM.addEventListener("click", clickEvent)
 
 function clickEvent() {
     console.log(this.id)
-    if (this.id === "increase") {
-        counter++
-    } else {
-        counter--
-    }
+    console.log(typeof (counter))
+    if (this.id === "increase" ? counter+= 1 : counter-= 1)
+    localStorage.setItem("counter", counter)
     counterDOM.innerHTML = counter
 }
 
+
+
 // increaseDOM.addEventListener('click', () => {
 //     counter++
+//     localStorage.setItem("counter", counter)
 //     counterDOM.innerHTML = counter
-// })
+// }
+// )
 
 // decreaseDOM.addEventListener('click', () => {
 //     counter--
+//     localStorage.setItem("counter", counter)
 //     counterDOM.innerHTML = counter
 // })
